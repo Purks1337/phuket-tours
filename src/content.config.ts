6f91ad59324f2c69d062c10ad3efc690c384c258
Purks1337 <Purks@ya.ru>
2026-03-04 23:55:10 +0500

    Bounty Tours MVP

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,14 +3,14 @@ import { glob } from 'astro/loaders';
 
 export const collections = {
   excursions: defineCollection({
-    // Загружаем все .md файлы из папки excursions
     loader: glob({ pattern: "**/*.md", base: "./src/content/excursions" }),
-    // Описываем схему данных (Zod)
-    schema: z.object({
+    // Обрати внимание: мы получаем helper 'image'
+    schema: ({ image }) => z.object({
       title: z.string(),
       category: z.string(),
       priceAdult: z.number(),
-      badges: z.array(z.string()).optional() // optional означает, что бейджиков может и не быть
+      badges: z.array(z.string()).optional(),
+      cover: image(), // Валидация и оптимизация картинки
     })
   })
 };
